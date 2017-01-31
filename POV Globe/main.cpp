b594eb61ff3a19521537c0b5c1fe785cd47fe2f2
Aaron Groom <agr00m@users.noreply.github.com>
2017-01-31 15:53:43 -0600

    Optimized calculations & added image rotation
    Modified a lot of the calculations to use bitwise operations. This drastically reduced overhead and allowing the clock to run at full speed (no prescaler). Also added

--- POV Globe/main.cpp
+++ POV Globe/main.cpp
@@ -3,14 +3,14 @@
  *********************************************************
  *   Author: Aaron Groom                                 *
  *  Created: 10/21/2016 8:40 AM                          *
- *	Modified: 09/28/2014 10:00 AM                         *
+ *	Modified: 01/31/2017 03:00 PM                         *
  *	 Version: 0.1                                         *
  *********************************************************
  * Project Design
  *
  *  Processor:  ATMEGA328P
  *
- *	Inputs:
+ *	 Inputs:
  *			PD2 - INT0 - Reed switch
  *
  *  Outputs:
@@ -43,12 +43,18 @@
  *
  *******************************************************/
 #define F_CPU 16000000UL	// 16 MHz Clock
+#define PRESCALER 1        // Increase for slower 
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "pumpkin2-hex.h"
 
-// BITWISE MACROS
+/* Uncomment image to display */
+//#include "pumpkin2-hex.h"
+//#include "star.h"
+//#include "snowflake.h"
+#include "snowflakes.h"
+
+/* ----- BITWISE MACROS ----- */
 #define SETBIT(ADDRESS,BIT) (ADDRESS |= (1<<BIT))
 #define CLEARBIT(ADDRESS,BIT) (ADDRESS &= ~(1<<BIT))
 #define CHECKBIT(ADDRESS,BIT) (ADDRESS & (1<<BIT))
@@ -61,64 +67,91 @@ void rowsOff(void);
 void columnsOff(void);
 void setColumn(uint8_t, uint8_t);
 void setRow(uint8_t, uint8_t);
-
 void TC0_Enable(uint8_t);
-uint8_t TC0_ReadReset(void);
 void TC0_Write(uint8_t);
-
+uint8_t TC0_ReadReset(void);
 void TC1_Enable(uint8_t);
-uint16_t TC1_Read(void);
 void TC1_Write(uint16_t);
+uint16_t TC1_Read(void);
+uint16_t TC1_ReadReset(void);
 
 /* ----- GLOBAL VARIABLES ----- */
-uint16_t t0OverflowCounter = 0;     // Timer0 overflow counter
-uint8_t currentImageColumn = 0;     // Image column to display
+volatile uint16_t overflowCounter = 0;       // Overflow counter
+volatile uint8_t currentImageColumn = 0;     // Image column to display
+volatile uint8_t columnOffset = 0;           // Holds the column offset for rotating images
+volatile uint8_t rotateSpeed = 1;            // Rotation speed, 0 = off
 
-/************************************************************************/
-/* External Interrupt 0                                                 */
-/************************************************************************/
-/* INT0 called when reed switch is activated. The # of clock cycles     */
-/* from the previous interrupt are counted, then divided by the number  */
-/* of total image columns then set to Timer1's CTC TOP value. Timer1,   */
-/* Timer1 overflow counter and current column counter are reset to 0.   */
-/************************************************************************/
+/************************************************************************
+ * External Interrupt 0                                                 *
+ ************************************************************************
+ * INT0 called when reed switch is activated. The # of clock cycles     *
+ * from the previous interrupt are counted, then divided by the number  *
+ * of total image columns then set to Timer1's CTC TOP value. Timer1 &  *
+ * T1 OF counter are reset to 0, and current image column counter is    *
+ * set to the column offest (for rotating image).                       *
+ ************************************************************************/
 ISR(INT0_vect) {
-   if (t0OverflowCounter > 30) {
-      OCR1A = ((((256 * t0OverflowCounter) + TC0_ReadReset())) / imageColumns) - 1;  // Set Timer1 Output Compare Match value
-      TC1_Write(0);
-      currentImageColumn = 0;    // Reset current column
-      t0OverflowCounter = 0;     // Reset Timer0 overflow counter
+   // Dirty attempt to manage switch debounce else
+   // the timing calculation for OCR1A will be off.
+   if (overflowCounter > 30) {               
+      
+      // Calculate the time each column is displayed
+      OCR1A = ((((unsigned long)overflowCounter - 1) << 8) | 
+               (unsigned long)TC0_ReadReset()) / imageColumns;
+      
+      // Reset everything
+      TC1_Write(0);                          // Reset timer1 to 0
+      overflowCounter = 0;                   // Reset Timer0 overflow counter
+      currentImageColumn = columnOffset;     // Reset current column to offest
+
+      // Rotates the image by incrementing the column offset.
+      if ((columnOffset+rotateSpeed) < imageColumns) {
+         columnOffset += rotateSpeed;
+      } else {
+         columnOffset = columnOffset + rotateSpeed - imageColumns;
+      }
    }
 }
 
-/************************************************************************/
-/* Timer0 Overflow Interrupt                                            */
-/************************************************************************/
-/* Increment timer 0 overflow counter.                                  */
-/************************************************************************/
-ISR(TIMER0_OVF_vect){
-   t0OverflowCounter++;
+/************************************************************************
+ * Timer0 Overflow Interrupt                                            *
+ ************************************************************************
+ * Increment timer 0 overflow counter.                                  *
+ ************************************************************************/
+ISR(TIMER0_OVF_vect) {
+   overflowCounter++;
 }
 
-/************************************************************************/
-/* Timer1 Output Compare Match Interrupt                                */
-/************************************************************************/
-/* Increments the current display column                                */
-/************************************************************************/
+/************************************************************************
+ * Timer1 Output Compare Match Interrupt                                *
+ ************************************************************************
+ * Increments the current column to display.                            *
+ ************************************************************************/
 ISR(TIMER1_COMPA_vect) {
-   currentImageColumn++;
+   if (currentImageColumn < imageColumns-1) {
+      currentImageColumn++;
+   } else {
+      currentImageColumn = 0;
+   }
 }
 
+/************************************************************************
+ * Main                                                                 *
+ ************************************************************************
+ * Continually cycles through each LED and displays the values stored   *
+ * in image[currentImageColumn][n]. Image column values are updated via *
+ * the timer1 compare interrupt.                                        *
+ ************************************************************************/
 int main(void) {
    uint8_t rowValue = 0xFF;
    init();
    while (1) {
-      for (uint8_t col = 0; col < 10; col++) {           // Loop through each block of 8 LEDs (columns)
+      for (uint8_t col = 0; col < ledEights; col++) {    // Loop through each block of 8 LEDs (columns)
          rowValue = image[currentImageColumn][col];      // Read value for block of 8 LEDs
          setColumn(col, 0);                              // Enable column
          for (uint8_t row = 0; row < 8; row++) {         // Loop through each LED (rows)
-            if ((rowValue & (1<<row)) >> row) {
-               setRow(row, 1);
+            if ((rowValue & (1<<row)) >> row) {          // Check if current value is 1
+               setRow(row, 1);                           // Turn the row on
             }
             setRow(row, 0);                              // Turn LED off
          }
@@ -127,17 +160,17 @@ int main(void) {
    }
 }
 
-/************************************************************************/
-/* init                                                                 */
-/************************************************************************/
-/* Initialize I/O pins, timers and interrupts and starts timer 0 & 1.   */
-/************************************************************************/
+/************************************************************************
+ * init                                                                 *
+ ************************************************************************
+ * Initialize I/O pins, timers and interrupts and starts timers 0 & 1.  *
+ ************************************************************************/
 void init(void){
    
    // Setup GPIO data direction
-   DDRB  |= 0b00111111;	   // DDRB - PB0-PB5 Output
-   DDRC  |= 0b00011111;	   // DDRC - PC0-PC5 OUtput
-   DDRD   = 0b11111011;	   // DDRD - PD0-PD1 & PD3-PD7 Output, PD2 Input
+   DDRB  |= 0b00111111;	   // DDRB: PB0-PB5 Output
+   DDRC  |= 0b00011111;	   // DDRC: PC0-PC5 OUtput
+   DDRD   = 0b11111011;	   // DDRD: PD0-PD1 & PD3-PD7 Output, PD2 Input
    SETBIT(PORTD, PD2);	   // Enable pull-up on PD2(INT0)
 
    rowsOff();              // Set rows LOW
@@ -151,30 +184,35 @@ void init(void){
       
    SREG   |= 0b10000000;   // Global Interrupt Enable
 
-   TC0_Enable(3);          // Start Timer 0, clk/64 Prescaler
-   TC1_Enable(3);          // Start Timer 1, clk/64 Prescaler
+   TC0_Enable(PRESCALER);  // Start Timer 0, clk/n Prescaler
+   TC1_Enable(PRESCALER);  // Start Timer 1, clk/n Prescaler
 }
 
-/************************************************************************/
-/* rowsOff                                                              */
-/************************************************************************/
-/* Sets all row I/O pins to 0.                                          */
-/************************************************************************/
+/************************************************************************
+ * rowsOff                                                              *
+ ************************************************************************
+ * Sets all row I/O pins to 0.                                          *
+ ************************************************************************/
 void rowsOff(void) {
    PORTC &= 0xFE;    // 0b11111110 PC0 LOW              
    PORTD &= 0x04;    // 0b00000100 PD0-PD1, PD3-PD7 LOW 
 }
 
-/************************************************************************/
-/* columnsOff                                                           */
-/************************************************************************/
-/* Sets all column I/O pins to 1.                                       */
-/************************************************************************/
+/************************************************************************
+ * columnsOff                                                           *
+ ************************************************************************
+ * Sets all column I/O pins to 1.                                       *
+ ************************************************************************/
 void columnsOff(void) {
    PORTB |= 0x3F;    // 0b00111111 PB0-PB5 HIGH
    PORTC |= 0x1E;    // 0b00011110 PC1-PC4 HIGH
 }
 
+/************************************************************************
+ * setColumn                                                            *
+ ************************************************************************
+ * Sets or resets an individual column.                                 *
+ ************************************************************************/
 void setColumn(uint8_t col, uint8_t val) {
    if (val == 1) {
       switch(col) {
@@ -205,6 +243,11 @@ void setColumn(uint8_t col, uint8_t val) {
    }
 }
 
+/************************************************************************
+ * setRow                                                               *
+ ************************************************************************
+ * Sets or resets an individual row.                                    *
+ ************************************************************************/
 void setRow(uint8_t row, uint8_t val) {
    if (val == 1) {
       switch(row) {
@@ -233,14 +276,14 @@ void setRow(uint8_t row, uint8_t val) {
 
 /* ----- TIMER/COUNTER 0 ----- */
 
-/************************************************************************/
-/* TC0_Enable                                                           */
-/************************************************************************/
-/* Starts and stops Timer/Counter 0 based on passed prescaler value.    */
-/* 0: Off | 1: clk/1 | 2: clk/8 | 3: clk/64 | 4: clk/256 | 5: clk/1-24  */
-/* 6: External falling edge | 7: External rising edge.                  */
-/* Invalid prescaler disables timer.                                    */
-/************************************************************************/
+/************************************************************************
+ * TC0_Enable                                                           *
+ ************************************************************************
+ * Starts and stops Timer/Counter 0 based on passed prescaler value.    *
+ * 0: Off | 1: clk/1 | 2: clk/8 | 3: clk/64 | 4: clk/256 | 5: clk/1-24  *
+ * 6: External falling edge | 7: External rising edge.                  *
+ * Invalid prescaler disables timer.                                    *
+ ************************************************************************/
 void TC0_Enable(uint8_t prescaler) {
    CLEARBITMASK(TCCR0B, 0x03);
    if (prescaler > 0 && prescaler < 8) {
@@ -248,26 +291,36 @@ void TC0_Enable(uint8_t prescaler) {
    }
 }
 
+/************************************************************************
+ * TC0_ReadReset                                                        *
+ ************************************************************************
+ * Reads Timer/Counter0 and resets the value to zero.                   *
+ ************************************************************************/
 uint8_t TC0_ReadReset(void) {
    uint8_t TC0 = TCNT0;
    TC0_Write(0);
    return TC0;
 }
 
+/************************************************************************
+ * TC1_Write                                                            *
+ ************************************************************************
+ * Writes the passed value to Timer/Counter0.                           *
+ ************************************************************************/
 void TC0_Write(uint8_t value) {
    TCNT0 = value;
 }
 
 /* ----- TIMER/COUNTER 1 ----- */
 
-/************************************************************************/
-/* TC1_Enable                                                           */
-/************************************************************************/
-/* Starts and stops Timer/Counter 1 based on passed prescaler value.    */
-/* 0: Off | 1: clk/1 | 2: clk/8 | 3: clk/64 | 4: clk/256 | 5: clk/1-24  */
-/* 6: External falling edge | 7: External rising edge.                  */
-/* Invalid prescaler disables timer.                                    */
-/************************************************************************/
+/************************************************************************
+ * TC1_Enable                                                           *
+ ************************************************************************
+ * Starts and stops Timer/Counter 1 based on passed prescaler value.    *
+ * 0: Off | 1: clk/1 | 2: clk/8 | 3: clk/64 | 4: clk/256 | 5: clk/1-24  *
+ * 6: External falling edge | 7: External rising edge.                  *
+ * Invalid prescaler disables timer.                                    *
+ ************************************************************************/
 void TC1_Enable(uint8_t prescaler) {
    if (prescaler == 0) {
       CLEARBITMASK(TCCR1B, 0x03);
@@ -276,23 +329,32 @@ void TC1_Enable(uint8_t prescaler) {
    }
 }
 
-/************************************************************************/
-/* TC1_Write                                                            */
-/************************************************************************/
-/* Sets Timer/Counter 1 to passed value.                                */
-/************************************************************************/
+/************************************************************************
+ * TC1_Write                                                            *
+ ************************************************************************
+ * Writes the passed value to Timer/Counter1.                           *
+ ************************************************************************/
 
 void TC1_Write(uint16_t value) {
-   //TCNT1H = value >> 8;
-   //TCNT1L = value & 0xF0;
    TCNT1 = value;
 }
 
-/************************************************************************/
-/* TC1_Read                                                             */
-/************************************************************************/
-/* Sets Timer/Counter 1 to passed value.                                */
-/************************************************************************/
+/************************************************************************
+ * TC1_Read                                                             *
+ ************************************************************************
+ * Reads Timer/Counter1.                                                *
+ ************************************************************************/
 uint16_t TC1_Read() {
    return TCNT1;
+}
+
+/************************************************************************
+ * TC1_ReadReset                                                        *
+ ************************************************************************
+ * Reads Timer/Counter1 and resets the value to zero.                   *
+ ************************************************************************/
+uint16_t TC1_ReadReset(void) {
+   uint16_t TC1 = TCNT1;
+   TC1_Write(0);
+   return TC1;
 }
